67c70f06209fbbc37fa2b7380176cb60d9c1792a
geo5555 <geo5555@users.noreply.github.com>
2019-11-02 10:22:17 +0200

    edited counter

--- analyze_log3_counter.py
+++ analyze_log3_counter.py
@@ -1,16 +1,15 @@
 import re
 import sys
-from collections import Counter, defaultdict
+from collections import Counter
 import pprint
 
 with open(sys.argv[1]) as f:
-    events = []
+    c = Counter()
     while(not re.search(r"^Log Buffer", next(f), flags=re.IGNORECASE)):
         pass   
     next(f)
     for line in f:
-        event = line.split(":")[3].strip()
-        events.append(event)
-    c = Counter(events)
+        event = line.split(":")[3].strip()[1:]
+        c[event] +=1
     pprint.pprint(c)
     pprint.pprint(c.most_common(10))
